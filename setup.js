5414aa18ff464dacbb04f50c4b5e0a9e1762389e
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2021-11-12 11:48:47 -0600

    Update setup.js

--- setup.js
+++ setup.js
@@ -5,6 +5,6 @@ const chromedriverPath = process.argv[2];
 
 if(typeof chromedriverPath === 'undefined') return;
 
-const content = `CHROMEDRIVER_PATH="${chromedriverPath}"`;
+const content = `CHROMEDRIVER_PATH='${chromedriverPath}'`;
 
 fs.writeFileSync(path.resolve(__dirname, '.env'), content);
