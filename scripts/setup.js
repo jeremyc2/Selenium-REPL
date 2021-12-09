b5c0ca07f605ee5491a37354456a844da087cab5
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2021-12-09 11:38:53 -0600

    Update setup.js

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,10 +1,10 @@
 const fs = require('fs'),
     path = require('path');
 
-const chromedriverPath = process.argv[2];
+const driverPath = process.argv[2];
 
-if(typeof chromedriverPath === 'undefined') return;
+if(typeof driverPath === 'undefined') return;
 
-const content = `CHROMEDRIVER_PATH='${chromedriverPath}'`;
+const content = `DRIVER_PATH='${driverPath}'`;
 
 fs.writeFileSync(path.resolve(__dirname, '../.env'), content);
