bdbf7125ed42f925df0b2fa8360aa9d1824fe9bd
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2021-12-10 11:31:38 -0600

    Update installGeckodriver.js

--- scripts/installGeckodriver.js
+++ scripts/installGeckodriver.js
@@ -4,4 +4,4 @@ const { spawn } = require('child_process');
 var args = process.argv.slice(2),
     opts = {cwd: __dirname, stdio: 'inherit', shell: process.platform === 'win32'? 'powershell.exe': 'pwsh'};
 
-spawn('./Install-Geckodriver.ps1', args, opts);
+spawn('../Install-Geckodriver.ps1', args, opts);
