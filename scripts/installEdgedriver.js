f8fc53e6326e8d165dfcaf836d566e3aa77260a7
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2021-12-10 11:31:21 -0600

    Update installEdgedriver.js

--- scripts/installEdgedriver.js
+++ scripts/installEdgedriver.js
@@ -4,4 +4,4 @@ const { spawn } = require('child_process');
 var args = process.argv.slice(2),
     opts = {cwd: __dirname, stdio: 'inherit', shell: process.platform === 'win32'? 'powershell.exe': 'pwsh'};
 
-spawn('./Install-Edgedriver.ps1', args, opts);
+spawn('../Install-Edgedriver.ps1', args, opts);
