612cf3b46cca628993f2b4b124d81acf248ad8cc
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2021-12-10 11:30:53 -0600

    Update installChromedriver.js

--- scripts/installChromedriver.js
+++ scripts/installChromedriver.js
@@ -4,4 +4,4 @@ const { spawn } = require('child_process');
 var args = process.argv.slice(2),
     opts = {cwd: __dirname, stdio: 'inherit', shell: process.platform === 'win32'? 'powershell.exe': 'pwsh'};
 
-spawn('./Install-Chromedriver.ps1', args, opts);
+spawn('../Install-Chromedriver.ps1', args, opts);
